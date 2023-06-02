d14354271b75b8de55b59914352c4b59a5c47a5f
MacKetchup <stylesthestyer@gmail.com>
2023-06-01 21:03:14 -0700

    changed user model to have a username based signup and login, (called cyptnonym) and adjusted client pages and utils, as well as server models, schemas, and utils to reflect that change

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -23,8 +23,7 @@ const typeDefs = gql`
 
   type User {
     _id: ID
-    firstName: String
-    lastName: String
+    cryptonym: String
     email: String
     orders: [Order]
   }
@@ -48,9 +47,9 @@ const typeDefs = gql`
   }
 
   type Mutation {
-    addUser(firstName: String!, lastName: String!, email: String!, password: String!): Auth
+    addUser(cryptonym:String!, email: String!, password: String!): Auth
     addOrder(products: [ID]!): Order
-    updateUser(firstName: String, lastName: String, email: String, password: String): User
+    updateUser(cryptonym: String!, email: String, password: String): User
     updateProduct(_id: ID!, quantity: Int!): Product
     login(email: String!, password: String!): Auth
   }
--- server/utils/auth.js
+++ server/utils/auth.js
@@ -26,8 +26,8 @@ module.exports = {
 
     return req;
   },
-  signToken: function ({ firstName, email, _id }) {
-    const payload = { firstName, email, _id };
+  signToken: function ({ cryptonym, email, _id }) {
+    const payload = { cryptonym, email, _id };
 
     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
   },
