56f616f3ed283d414238823913a1cbe2f0c8663c
mvill <119754846+migavg@users.noreply.github.com>
2023-06-07 21:21:23 -0700

    fixed bugs that prevented footer from being at bottom of page along with product page bugs and got started on styling order history page

--- client/src/pages/OrderHistory.js
+++ client/src/pages/OrderHistory.js
@@ -13,26 +13,26 @@ function OrderHistory() {
   }
 
   return (
-    <>
-      <div className="container my-1">
-        <Link to="/">← Back to Products</Link>
+    <div>
+      <div className="container has-background-black mt-6 p-6">
+        <Link className="has-text-white" to="/">← Back to Products</Link>
 
         {user ? (
-          <>
-            <h2>
+          <div className="p-2">
+            <h2 className="has-text-white is-size-3">
               Order History for {user.cryptonym}
             </h2>
             {user.orders.map((order) => (
-              <div key={order._id} className="my-2">
+              <div key={order._id} className="has-text-white">
                 <h3>
-                  {new Date(parseInt(order.purchaseDate)).toLocaleDateString()}
+                   Date purchased: {new Date(parseInt(order.purchaseDate)).toLocaleDateString()}
                 </h3>
-                <div className="flex-row">
+                <div className=" is-flex is-flex-wrap-wrap">
                   {/* add story and paywall feilds to args */}
                   {order.products.map(({ _id, name, price, story, paywall }, index) => (
-                    <div key={index} className="card px-1 py-1">
+                    <div key={index} className="card mb-6">
                       <Link to={`/products/${_id}`}>
-                        <p>{name}</p>
+                        <p className="is-size-4 has-text-white has-text-centered">{name}</p>
                       </Link>
                       {/* <div>
                         <span>${price}</span>
@@ -44,7 +44,7 @@ function OrderHistory() {
                         </div>
                         ) : (
                           <div>
-                            <p>{story}</p>
+                            <p className="has-text-centered">{story}</p>
                           </div>
                       )}
                     </div>
@@ -52,10 +52,10 @@ function OrderHistory() {
                 </div>
               </div>
             ))}
-          </>
+          </div>
         ) : null}
       </div>
-    </>
+    </div>
   );
 }
 
