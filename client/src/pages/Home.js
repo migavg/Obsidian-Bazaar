df46479909cfd3ba02a1e7cb225e81b2033164dd
Cole <ce.kasabian@gmail.com>
2023-06-08 23:03:10 -0700

    completed the success parameter

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ProductList from "../components/ProductList";
 import CategoryMenu from "../components/CategoryMenu";
 import Cart from "../components/Cart";
 import Hero from "../components/Hero";
-
+import { redirect, useSearchParams, useNavigate } from "react-router-dom";
+import Success from "./Success";
 
 const Home = () => {
+  const [queries, setQueries] = useSearchParams();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (queries.get("success")) {
+      navigate(`/success?session_id=${queries.get("session_id")}`);
+    }
+  }, [queries]);
+
   return (
     <div>
       <Hero />
